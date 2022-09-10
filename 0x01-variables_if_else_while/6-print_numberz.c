2f9e849add1b87a0f64cfea5d57377a227822d96
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-10 03:17:47 -0700

    Task 6 C variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints single numbers of base 10 starting from 0
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar("%d" i);
+	}
+
+	putchar('\n');
+	return 0;
+}
