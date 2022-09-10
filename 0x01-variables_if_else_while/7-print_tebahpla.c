1c4efc4ae9cb4e101b06d0912ab22477bb701897
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-10 03:59:05 -0700

    Task 7 C variables

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints lowercase alphabets in reverse order
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	
+	int i;
+
+	for (i = '26'; i = '1'; i--)
+	{
+		putchar(i);
+	}
+	putchar("\n")
+	return (0);
+}
