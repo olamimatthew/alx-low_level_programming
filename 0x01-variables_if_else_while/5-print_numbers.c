6a67effcd947193462f858d6442b02474d05db05
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-10 02:46:57 -0700

    Task 5 C variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	Return (0);
+}
