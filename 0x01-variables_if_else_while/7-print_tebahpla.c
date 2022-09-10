39e92a1e1aec42d7f4ee258a9ffc971c52b2f0de
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-10 04:11:44 -0700

    Task 7 C variables

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,11 +10,11 @@
 
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char i[26] = "abcdefghijklmnopqrstuvwxyz";
 	
 	int i;
 
-	for (i <= 26; i > '1'; i--)
+	for (i = 26; i > 1; i--)
 	{
 		putchar(i);
 	}
