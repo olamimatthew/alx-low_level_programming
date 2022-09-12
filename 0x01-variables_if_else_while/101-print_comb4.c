a56e0607c511dd2343bfe9e599c499bea1d60e87
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-12 14:33:16 -0700

    Task 11 C variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 
 {
 	int i, j, k;
+
 	for (i = '0'; i < '9'; i++)
 	{
-		for (j = i + 1; j <='9'; j++)
+		for (j = i + 1; j <= '9'; j++)
 		{
 			for (k = j + 1; k <= '9'; k++)
 				if ((i != j) != k)
@@ -14,7 +21,7 @@ int main(void)
 				putchar(i);
 				putchar(j);
 				putchar(k);
-				if(i == '7' && j == '8')
+				if (i == '7' && j == '8')
 					continue;
 				{
 					putchar(',');
