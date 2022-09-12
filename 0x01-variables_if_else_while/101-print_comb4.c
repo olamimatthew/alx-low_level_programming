48862495b6af6ca16a0a9153fbcffcc42bcdbd5d
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-12 14:28:37 -0700

    Task 11 C variables

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+	int i, j, k;
+	for (i = '0'; i < '9'; i++)
+	{
+		for (j = i + 1; j <='9'; j++)
+		{
+			for (k = j + 1; k <= '9'; k++)
+				if ((i != j) != k)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if(i == '7' && j == '8')
+					continue;
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
