48f5032c443edef3a2cf90e8c48a41c451bebb28
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-12 05:26:44 -0700

    Task 8 C variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints the digits of base 16
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 28; a < 38; a++)
+	{
+		putchar(a);
+	}
+	for (a = 67; a < 73; a++)
+	{
+		putchar(a);
+	}
+
+	putchar("\n");
+	return (0);
+}
+
