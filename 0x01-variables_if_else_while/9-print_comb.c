088f4a2d20949d9fb588d68c5661ac14905acaec
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-12 06:12:37 -0700

    Task 9 C variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,8 +15,11 @@ int main(void)
 	for (a = 48; a < 58; a++)
 	{
 		putchar(a);
-		putchar(',');
-		putchar(' ');
+		for (a < 57;)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
