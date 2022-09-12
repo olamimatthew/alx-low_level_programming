f589a8b7fde660107cc0f910fe60d400b72aa879
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-12 05:59:53 -0700

    Task 9 C variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+		for (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
