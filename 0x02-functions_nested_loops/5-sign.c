d533f57c4055a628fcb3c2c419820ded5229fd42
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-13 16:23:10 -0700

    Task 5 C functions and nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,27 +5,27 @@
  * Returns 0 and prints 0 if n is zero
  * Returns -1 and prints - if n is less than zero
  *
- * @c: the character in ASCII code
+ * @c: the input number as an integer
  *
  * Return: 1 if number is greater than zero, 0 if number is zero
  * -1 if number is less than zero
  */
 int print_sign(int n)
 {
-	if (n >= 48 && n <= 57)
+	if (n > 0)
 	{
+		_putchar(43);
 		return (1);
-		_putchar(+);
 	}
-	if (n == 48)
+	else if (n < 0)
 	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
 		return (0);
-		_putchar(0);
-	}
-	if (n < 48)
-	{
-		return(-1);
-		_putchar(-);
 	}
 	_putchar('\n');
 
