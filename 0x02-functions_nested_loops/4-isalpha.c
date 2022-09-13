4dc4a9344f6d53fb4805677eafa2da29a3269dc7
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-13 15:50:26 -0700

    Task 4 C functions and nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,12 @@
 #include "main.h"
 
 /**
- * _isalpha - Returns 1 if c is a letter, lowercase or uppercase. Returns 0 otherwise
+ * _isalpha - Returns 1 if c is a letter, lowercase or uppercase. 
+ * Returns 0 otherwise
  *
+ * @c: the character in ASCII code
+ *
+ * Return: 1 for letters, 0 otherwise
  */
 
 int _isalpha(int c)
