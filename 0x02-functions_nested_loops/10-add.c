ee73f8973ca4ce18626d50c005f0757f39f0f361
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-14 07:58:31 -0700

    Task 10 C functions and nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers together and returns the result
+ *
+ * @i: number one
+ * @j: number two
+ *
+ * Return: returns the result
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
