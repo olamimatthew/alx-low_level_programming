c2b9ae5495ef28e66a8cb7d86f76a8593f7c38f6
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-16 14:41:10 -0700

    Task 9 C more nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints number from 1 to 100
+ * 
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 1;
+	printf("%d", i);
+	for (i = 2; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf(" fizzbuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf(" fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf(" buzz");
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
