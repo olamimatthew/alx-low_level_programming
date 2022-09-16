f179ef909a30d1b19c187fb2b22c9cb2f3568340
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-16 08:14:04 -0700

    Task 5 C more nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers from 0 to 14 ten times
+ * Return: Nothing
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
