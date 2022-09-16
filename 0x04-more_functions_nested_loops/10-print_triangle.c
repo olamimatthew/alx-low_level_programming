fb2c572c2b9b77006c572fcb19862298dea9f866
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-16 15:25:06 -0700

    Task 10 C more nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,11 +12,17 @@ void print_triangle(int size)
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < (size - i); j++)
+		{
 			_putchar(' ');
+		}
 		for (j--; j < size; j++)
+		{
 			_putchar(35);
+		}
 		if (i < (size - i))
+		{
 			_putchar('\n');
+		}
 	}
 	_putchar('\n');
 }
