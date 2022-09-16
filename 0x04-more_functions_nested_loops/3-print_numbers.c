a77a7a63ddbd051fa6b48b5ffff96a72ea0da0c2
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-16 03:54:33 -0700

    Task 3 C more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ * @a: character to print numbers
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '8'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
