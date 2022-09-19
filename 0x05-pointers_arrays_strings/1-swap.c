206997b04d73baff73f25c6419959dbdee2e927b
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-19 13:25:30 -0700

    Task 1 C pointers and arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two intergers between themselves
+ * @a: first interger
+ * 2b: second interger
+ */
+
+void swap_int(int *a, int *b);
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
