978a5b711bf466507174abdaacfd2bb0f298e570
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-19 13:43:34 -0700

    Task 2 C pointers and arrays

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: input string
+ * Return: Learn of a string.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+		count++;
+	return (count;
+}
