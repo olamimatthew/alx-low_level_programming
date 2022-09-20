346b49173d90743cbc9c866eb853d7b46d66a0be
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-19 20:27:41 -0700

    Task 4 C pointers and arrays

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse order followed by a new line
+ * @s: input string.
+ * Return: Nothing.
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+		{
+			_putchar('\n');
+				break;
+			count++;
+		}
+		for (count--; count >= 0; count--)
+			_putchar(s[count]);
+		_putchar('\n');
+	}
+}
