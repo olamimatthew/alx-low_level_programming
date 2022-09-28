c7fea5bf4fd6a565f40ef7a209ba6dd58c2a3aae
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-09-28 11:32:23 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Prints the value of x raise to y
+ * @x: first integer base
+ * @y: second integer exponent
+ * Return: The value of exponentiation
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y -1));
+}
