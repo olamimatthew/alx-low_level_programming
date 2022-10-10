c5c25e421cdc921ddf85bf13eb268f6725213ae5
Ogungbade Olamilekan <olamimatthew120@gmail.com>
2022-10-10 14:33:16 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,6 +25,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (lname = 0; name[lname]; lname++)
 		;
 
+	for (lowner = 0; owner[lowner]; lowner++)
+		;
+
 	p_dog->name = malloc(lname + 1);
 	p_dog->owner = malloc(lowner + 1);
 
