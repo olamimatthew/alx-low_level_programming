2713b411fbb221107828f98ae1fd969d0b965362
Olamilekan Ogungbade <olamimatthew120@gmail.com>
2022-12-13 00:11:10 -0800

    Task 2 files

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include "op.h"
+
+/**
+ * add - Adds two integers
+ * @a: first integer
+ * @b: Second integer
+ *
+ * Return: Result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Subtracts two integers
+ * @a: first integer
+ * @b: Second integer
+ *
+ * Return: Result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: The result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divided two integers and return the result
+ * @a: Numerator
+ * @b: denominator
+ *
+ * Return: The result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Finds the remainder of division of two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: The result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
